d191d6e14a118fbe487f792196ba2e326c42f5d9
ysys1007 <yscho071017@gmail.com>
2023-07-24 22:03:13 +0900

    login profile second update

--- Pages/src/components/login.vue
+++ Pages/src/components/login.vue
@@ -1,35 +1,127 @@
+
 <template>
     <div id="login">
-        <div class="welcome">Welcome to<br>COCACO!</div>
+        <div class="welcome">Welcome to COCACO!</div>
         <div class="signin">Sign In</div>
+        <div class="signinbackground"></div>
+        <div>
+            <input class="ID" type=text name="id" placeholder="ID">
+            <br><br>
+            <input class="PS" type=text name="ps" placeholder="PassWord">
+        </div>
+        <button>SIGN IN</button>
+        <!-- v-on:click="OnClickButton" -->
+        <img src="">
     </div>
 </template>
 
 <style>
-
+    * {
+        background-color: #f9c3e6;
+    }
     #login {
-        width:1000px;
+        overflow: hidden;
+        width:800px;
         height: 400px;
         background-color: #FFD4F0;
-        margin: 200px;
-        border-radius: 50px;
+        /* margin: 200px; */
+        border-radius: 10px;
+        position: relative;
+        top: 115px;
+        left: 370px; 
+        right: 50px;
+        filter: drop-shadow(10px 15px 10px rgba(0, 0, 0, 0.25));
+
     }
-    
 
     .welcome {
-        color: black;
+        background-color: #FFD4F0;
+        width: 517px;
+        height: 98px;
+        flex-shrink: 0;
+        color: #000;
+        text-align: center;
+        font-family: Inter;
         font-size: 50px;
-        font-family: 'Inter';
-        font-weight:600;
-        text-align: left;  
-        word-wrap: break-word;
-    }
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+        position: relative;
+        top: 135px;
+        right: 50px;
+    } 
 
+    
     .signin {
-        text-align: right;
-        font-family: 'Inter';
+        width: 241px;
+        height: 47px;
+        flex-shrink: 0;
+        color: #000;
+        background-color: white;
+        font-family: Inter;
+        font-size: 40px;
+        font-style: normal;
         font-weight: 700;
+        line-height: normal;
+        position:relative;
+        z-index: 1;
+        left: 475px;
+        bottom: 20px;
     }
 
+    .signinbackground {
+        background-color: white;
+        width: 500px;
+        height: 400px;
+        position:relative;
+        bottom: 145px;
+        left: 400px;
 
+    }
+    
+    input {
+        background-color: #D9D9D9;
+        opacity: 0.7;
+        font-family: Inter;
+        font-weight: 700;
+        width: 230px;
+        height: 30px;
+        position: relative;
+        bottom: 380px;
+        left: 203px;
+        overflow: hidden;
+        border-radius: 10px;
+    }
+
+    button {
+        width: 273px;
+        height: 44px;
+        font-family: Inter;
+        font-weight: 700;
+        font-size: 20px;
+        font-display: center;
+        flex-shrink: 0;
+        border-radius: 20px;
+        background: rgba(242, 124, 160, 0.52);
+        position:relative;
+        bottom: 350px;
+        right: -209px;
+    }
 </style>
+
+
+
+
+
+
+<!-- 
+<script>
+    const app = new Vue ({
+        el: '#login',
+        methods: {
+            OnClickButton() {
+                console.log('SIGN IN!!!!')
+            },
+        },
+    });
+</script> -->
--- Pages/src/components/profile.vue
+++ Pages/src/components/profile.vue
@@ -1,8 +1,11 @@
 <template>
     <div id="profile">
-        <div class="img">사진</div>
+        <div class="img">
+            <img src="https://pbs.twimg.com/media/EbviXFOU4AUv1sP.jpg" width="270px" height="270px">
+        </div>
         <button class="upload">사진 업로드</button>
         <div class="name">이름</div>
+        <div class="namebackground"></div>
     </div>
 </template>
 
@@ -10,34 +13,58 @@
     #profile {
         width:700px;
         height: 400px;
+        background-clip: padding-box;
         background-color: #FFD4F0;
         margin: 200px;
         border-radius: 50px;
     }
 
     .img {
-        width: 300px;
-        height:300px;
+        width: 270px;
+        height:270px;
+        overflow: hidden;
+        background-clip: padding-box;
         background-color: #12000E;
         border-radius: 50%;
         position: relative;
-        top: 50px;
+        top: 70px;
         right: -40px;
 
     }
     .upload {
-        width: 300px;
-        height: 50px;
+        width: 200px;
+        height: 45px;
         border-radius: 30px;
-        background: rgba(208, 204, 207, 0.59);
+        background-clip: padding-box;
+        background: rgb(224, 224, 224);
         box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-        color: #775770;
+        color: black;
         flex-shrink: 0;
-        font-family: 'Inter';
-        font-size: 24px;
-        font-style: border;
+        font-family: Inter;
         font-weight: 500;
+        font-size: 20px;
         line-height: 30px;
+        position: relative;
+        top: -115px;
+        right: -85px;
     }
 
+    .name {
+        background-color: white;
+        width: 300px;
+        height: 40px;
+        border-radius: 30px;
+        flex-shrink: 0;
+        color: #000;
+        opacity: 0.7;;
+        font-family: Inter;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        position:relative;
+        left: 330px;
+        bottom: 90px; 
+    }
+    
 </style>
