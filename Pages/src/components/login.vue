4f7abc7faee6a752df5dcd080365b71ca539818d
ysys1007 <yscho071017@gmail.com>
2023-07-24 12:47:38 +0900

    login profile update

---
+++ Pages/src/components/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="login">
+        <div class="welcome">Welcome to<br>COCACO!</div>
+        <div class="signin">Sign In</div>
+    </div>
+</template>
+
+<style>
+
+    #login {
+        width:1000px;
+        height: 400px;
+        background-color: #FFD4F0;
+        margin: 200px;
+        border-radius: 50px;
+    }
+    
+
+    .welcome {
+        color: black;
+        font-size: 50px;
+        font-family: 'Inter';
+        font-weight:600;
+        text-align: left;  
+        word-wrap: break-word;
+    }
+
+    .signin {
+        text-align: right;
+        font-family: 'Inter';
+        font-weight: 700;
+    }
+
+
+</style>
---
+++ Pages/src/components/profile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="profile">
+        <div class="img">사진</div>
+        <button class="upload">사진 업로드</button>
+        <div class="name">이름</div>
+    </div>
+</template>
+
+<style>
+    #profile {
+        width:700px;
+        height: 400px;
+        background-color: #FFD4F0;
+        margin: 200px;
+        border-radius: 50px;
+    }
+
+    .img {
+        width: 300px;
+        height:300px;
+        background-color: #12000E;
+        border-radius: 50%;
+        position: relative;
+        top: 50px;
+        right: -40px;
+
+    }
+    .upload {
+        width: 300px;
+        height: 50px;
+        border-radius: 30px;
+        background: rgba(208, 204, 207, 0.59);
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        color: #775770;
+        flex-shrink: 0;
+        font-family: 'Inter';
+        font-size: 24px;
+        font-style: border;
+        font-weight: 500;
+        line-height: 30px;
+    }
+
+</style>
