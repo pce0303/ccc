29530bd236a6f2f1b0e4c021e86ae0a309247335
pce0303 <ivychoen@gmail.com>
2023-07-25 10:26:59 +0900

    when path '/' app -> Home

--- Pages/src/router/index.js
+++ Pages/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Post from '@/components/Post'
 
 Vue.use(Router)
@@ -9,8 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
     },
     {
       path: '/newpost',
