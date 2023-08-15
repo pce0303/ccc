ae0aee2623f20e31bb2ec70392ddf8fe8e36651b
pce0303 <ivychoen@gmail.com>
2023-08-15 21:51:50 +0900

    cocacopost -> cocaco_db

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'root',
-    password: '0303',
-    database: 'cocacopost'
+    user: 'cccuser',
+    password: '1234',
+    database: 'cocaco_db'
 });
 
 connection.connect((err) => {
