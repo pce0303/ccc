fb4e17fc4394535396eb6b58559c9c3b66aa84b3
ysys1007 <yscho071017@gmail.com>
2023-08-15 22:50:48 +0900

    ㅠㅠㅠㅠ 눙물의 라우터 업데이트..

---
+++ Pages/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import register from '@/components/register'
+import login from '@/components/login'
+import profile from '@/components/profile'
+import Home from '@/components/Home'
+import post from '@/components/post'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: register
+    },
+
+    {
+      path: '/login',
+      component: login
+    },
+
+    {
+      path: '/Home',
+      component: Home
+    },
+
+    {
+      path: '/profile',
+      component: profile
+    },
+    
+    {
+      path: '/new-post',
+      component: post
+    }
+
+  ]
+});
