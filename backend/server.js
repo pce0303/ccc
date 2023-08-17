a6f3a8dc8bff095c6e37504adee24c406e683b05
pce0303 <ivychoen@gmail.com>
2023-08-17 12:15:35 +0900

    add register router, login router, session

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 const path = require('path');
 const postRouter = require('./router/post');
 const commentRouter = require('./router/comment');
+const registerRouter = require('./router/register');
+const loginRouter = require('./router/login');
 
 app.listen(8080, ()=>{
     console.log('server on port 8080');
 })
 
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.use('/new-post', postRouter);
 app.use('/home', commentRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
-app.use( '/', express.static( path.join(__dirname, '../frontend/dist') ));  
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'app/dist/index.html'));  
+app.use(express.static( path.join(__dirname, '../frontend/dist') ));  
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));  
 })
